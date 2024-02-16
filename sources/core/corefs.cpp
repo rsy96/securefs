eb485c555f7f03479be152c699f27c0ef38824ba
Siyuan Ren <netheril96@gmail.com>
2024-02-17 00:02:43 +0800

    Implement name lookup

--- sources/core/corefs.cpp
+++ sources/core/corefs.cpp
@@ -1,15 +1,25 @@
 #include "core/corefs.hpp"
 
+#include "corefs.hpp"
 #include <SQLiteCpp/Exception.h>
+#include <absl/cleanup/cleanup.h>
 #include <absl/strings/str_split.h>
 #include <sqlite3.h>
 #include <utf8proc.h>
 
 namespace securefs
 {
-
 namespace
 {
+
+#ifdef _WIN32
+    const char kPathSep = '\\';
+#else
+    const char kPathSep = '/';
+#endif
+
+    constexpr CoreFileSystem::FileId kRootId = {};
+
     void register_utf8proc_map(sqlite3* db,
                                const char* name,
                                utf8proc_option_t map_option,
@@ -29,14 +39,14 @@ namespace
             new CustomFuncData{map_option, skip_same},
             [](sqlite3_context* context, int num_args, sqlite3_value** args)
             {
-                auto data = static_cast<const CustomFuncData*>(sqlite3_user_data(context));
                 if (num_args != 1)
                 {
                     sqlite3_result_error_code(context, SQLITE_MISUSE);
                     return;
                 }
+                auto data = static_cast<const CustomFuncData*>(sqlite3_user_data(context));
                 auto input = static_cast<const unsigned char*>(sqlite3_value_blob(args[0]));
-                if (!input)
+                if (!input || !data)
                 {
                     sqlite3_result_error_code(context, SQLITE_MISUSE);
                     return;
@@ -69,7 +79,32 @@ namespace
             throw SQLite::Exception(db, rc);
         }
     }
+    std::vector<std::string_view> map_and_split(std::string_view name, utf8proc_option_t map_option)
+    {
+        utf8proc_uint8_t* transformed = nullptr;
+        auto transformed_size = utf8proc_map(
+            reinterpret_cast<const uint8_t*>(name.data()), name.size(), &transformed, map_option);
+        if (transformed_size <= 0)
+        {
+            return {};
+        }
+        auto cleanup = absl::MakeCleanup([=]() { free(transformed); });
+        return absl::StrSplit(
+            std::string_view(reinterpret_cast<const char*>(transformed), transformed_size),
+            kPathSep);
+    }
 }    // namespace
+
+CoreFileSystem::CoreFileSystem(SQLite::Database db,
+                               const FileSystemInherentParams& inherent_params,
+                               const FileSystemMountParams& mount_params)
+    : db_(std::move(db))
+    , lookup_st_(db_, get_lookup_sql(mount_params.name_lookup_mode()))
+    , inherent_params_(inherent_params)
+    , mount_params_(mount_params)
+{
+    register_custom_functions();
+}
 
 void CoreFileSystem::initialize_tables()
 {
@@ -81,7 +116,8 @@ void CoreFileSystem::initialize_tables()
             ParentId BLOB NOT NULL,
             Name BLOB NOT NULL,
             FileId BLOB NOT NULL,
-            LinkCount INTEGER NOT NULL DEFAULT 1
+            FileType INT NOT NULL,
+            LinkCount INTEGER NOT NULL DEFAULT 1 CHECK(LinkCount > 0 AND LinkCount < 65536)
         );
 
         CREATE UNIQUE INDEX ParentAndName ON DirEntries (ParentId, Name);
@@ -96,7 +132,8 @@ void CoreFileSystem::initialize_tables()
             ParentId BLOB NOT NULL,
             Name BLOB NOT NULL,
             FileId BLOB NOT NULL,
-            LinkCount INTEGER NOT NULL DEFAULT 1,
+            FileType INT NOT NULL,
+            LinkCount INTEGER NOT NULL DEFAULT 1 CHECK(LinkCount > 0 AND LinkCount < 65536),
             CaseFoldedName TEXT AS (CASEFOLD_IF_CHANGED(Name)) STORED,
             UniNormedName TEXT AS (UNINORM_IF_CHANGED(Name)) STORED
         );
@@ -118,11 +155,115 @@ void CoreFileSystem::register_custom_functions()
     register_utf8proc_map(db_.getHandle(), "UNINORM_IF_CHANGED", UTF8PROC_COMPOSE, true);
 }
 
+void CoreFileSystem::throw_unsupported_name_lookup_mode(FileSystemMountParams::NameLookupMode mode)
+{
+    throw std::invalid_argument("Unsupported name lookup mode "
+                                + FileSystemMountParams::NameLookupMode_Name(mode));
+}
+
+const char* CoreFileSystem::get_lookup_sql(FileSystemMountParams::NameLookupMode mode)
+{
+    switch (mode)
+    {
+    case FileSystemMountParams::NAME_LOOKUP_EXACT:
+        return R"(
+            select FileId, LinkCount, FileType from DirEntries where ParentId = ?1 and Name = ?2;
+        )";
+
+    case FileSystemMountParams::NAME_LOOKUP_CASE_INSENSITVE:
+        return R"(
+            select FileId, LinkCount, FileType from DirEntries where ParentId = ?1 and 
+                (CaseFoldedName = ?2 or Name = ?2) limit 1;
+        )";
+
+    case FileSystemMountParams::NAME_LOOKUP_UNICODE_NORMED:
+        return R"(
+            select FileId, LinkCount, FileType from DirEntries where ParentId = ?1 and 
+                (UniNormedName = ?2 or Name = ?2) limit 1;
+        )";
+
+    default:
+        throw_unsupported_name_lookup_mode(mode);
+    }
+}
+
 CoreFileSystem::LookupResult CoreFileSystem::lookup(std::string_view name)
 {
     CoreFileSystem::LookupResult result;
+    std::vector<std::string_view> components;
 
+    switch (mount_params_.name_lookup_mode())
+    {
+    case FileSystemMountParams::NAME_LOOKUP_EXACT:
+        components = absl::StrSplit(name, '/');
+        break;
+
+    case FileSystemMountParams::NAME_LOOKUP_CASE_INSENSITVE:
+        components = map_and_split(name, UTF8PROC_CASEFOLD);
+
+    case FileSystemMountParams::NAME_LOOKUP_UNICODE_NORMED:
+        components = map_and_split(name, UTF8PROC_COMPOSE);
+        break;
+
+    default:
+        throw_unsupported_name_lookup_mode(mount_params_.name_lookup_mode());
+    }
+
+    if (components.empty())
+    {
+        return result;
+    }
+
+    FileId parent = kRootId;
+    for (size_t i = 0; i < components.size(); ++i)
+    {
+        auto cleanup = absl::MakeCleanup(
+            [&]() ABSL_NO_THREAD_SAFETY_ANALYSIS
+            {
+                lookup_st_.reset();
+                lookup_st_.clearBindings();
+            });
+        lookup_st_.bindNoCopy(1, parent.data(), parent.size());
+        lookup_st_.bindNoCopy(2, components[i].data(), components[i].size());
+        if (lookup_st_.executeStep())
+        {
+            auto c1 = lookup_st_.getColumn(1);
+            if (c1.getBytes() != parent.size())
+            {
+                throw std::runtime_error("Wrong ID size in the database");
+            }
+            result.parent_id = parent;
+            memcpy(parent.data(), c1.getBlob(), parent.size());
+            result.file_id.emplace(parent);
+            result.link_count = lookup_st_.getColumn(2).getInt();
+            result.file_type = static_cast<FileType>(lookup_st_.getColumn(3).getInt());
+            if (result.file_type == FileType::FILE_TYPE_UNSPECIFED
+                || !FileType_IsValid(result.file_type))
+            {
+                throw std::runtime_error("Invalid file type in the database");
+            }
+        }
+        else if (i + 1 != components.size())
+        {
+            throw NameLookupException();
+        }
+        else
+        {
+            result.parent_id = parent;
+            result.file_id.reset();
+            result.link_count = 0;
+            result.file_type = FileType::FILE_TYPE_UNSPECIFED;
+        }
+    }
+
+    auto last_sep_index = name.rfind(kPathSep);
+    result.last_component_name.assign(name.substr(last_sep_index + 1));
     return result;
+}
+
+const char* NameLookupException::what() const noexcept
+{
+    return "File name has non-existent component";
 }
 
 }    // namespace securefs
--- sources/core/corefs.hpp
+++ sources/core/corefs.hpp
@@ -3,11 +3,13 @@
 #include "protos/params.pb.h"
 
 #include <SQLiteCpp/Database.h>
+#include <SQLiteCpp/Statement.h>
 #include <SQLiteCpp/Transaction.h>
 #include <absl/base/thread_annotations.h>
 #include <absl/synchronization/mutex.h>
 
 #include <array>
+#include <exception>
 #include <optional>
 #include <string>
 #include <string_view>
@@ -20,38 +22,48 @@ class CoreFileSystem
 public:
     explicit CoreFileSystem(SQLite::Database db,
                             const FileSystemInherentParams& inherent_params,
-                            const FileSystemMountParams& mount_params)
-        : db_(std::move(db)), inherent_params_(inherent_params), mount_params_(mount_params)
-    {
-        register_custom_functions();
-    }
+                            const FileSystemMountParams& mount_params);
 
-    void initialize_tables() ABSL_EXCLUSIVE_LOCKS_REQUIRED(mutex());
+    void initialize_tables() ABSL_EXCLUSIVE_LOCKS_REQUIRED(mu_);
 
-    SQLite::Transaction transaction() ABSL_EXCLUSIVE_LOCKS_REQUIRED(mutex())
+    SQLite::Transaction transaction() ABSL_EXCLUSIVE_LOCKS_REQUIRED(mu_)
     {
         return SQLite::Transaction(db_);
     }
-    absl::Mutex& mutex() noexcept { return mu_; }
+    absl::Mutex& mutex() noexcept ABSL_LOCK_RETURNED(mu_) { return mu_; }
 
     using FileId = std::array<unsigned char, 32>;
 
     struct LookupResult
     {
-        FileId parent_id;
+        FileId parent_id{};
         std::string last_component_name;
         std::optional<FileId> file_id;
-        FileType file_type;
+        FileType file_type{};
+        int link_count = 0;
     };
 
-    LookupResult lookup(std::string_view name) ABSL_EXCLUSIVE_LOCKS_REQUIRED(mutex());
+    LookupResult lookup(std::string_view name) ABSL_EXCLUSIVE_LOCKS_REQUIRED(mu_);
+    void create(const LookupResult& item) ABSL_EXCLUSIVE_LOCKS_REQUIRED(mu_);
 
 private:
     absl::Mutex mu_;
-    SQLite::Database db_ ABSL_GUARDED_BY(mutex());
+    SQLite::Database db_ ABSL_GUARDED_BY(mu_);
+    SQLite::Statement lookup_st_ ABSL_GUARDED_BY(mu_);
     FileSystemInherentParams inherent_params_;
     FileSystemMountParams mount_params_;
 
-    void register_custom_functions();
+    void register_custom_functions() ABSL_LOCKS_EXCLUDED(mu_);
+
+    [[noreturn]] static void
+    throw_unsupported_name_lookup_mode(FileSystemMountParams::NameLookupMode mode);
+
+    static const char* get_lookup_sql(FileSystemMountParams::NameLookupMode mode);
+};
+
+class NameLookupException : public std::exception
+{
+public:
+    const char* what() const noexcept override;
 };
 }    // namespace securefs
